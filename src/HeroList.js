de0df1f111df76a2143b31ae9d8c25b1cc7ffc8a
Alexander Rovang <DXRovang@gmail.com>
2022-01-14 10:38:18 -0500

    sets up initial fetch in useEffect

--- src/HeroList.js
+++ src/HeroList.js
@@ -1,6 +1,17 @@
 import Hero from "./Hero"
-
+import { useState, useEffect } from 'react'
 const HeroList = () => {
+  const [dogs, setDogs] = useState([])
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/heros")
+    .then((r) => r.json())
+    .then((data) =>{
+      console.log(data)
+    }
+    )
+  })
+  
+  
   return ( 
     <div>
       In HeroList.js
